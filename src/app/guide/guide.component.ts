340b690a8e2e7961b1737b6b6c33a5be3bbf2deb
17sstmk <37619969+17sstmk@users.noreply.github.com>
2020-05-11 20:59:40 +0800

    a

---
+++ src/app/guide/guide.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guide',
+  templateUrl: './guide.component.html',
+  styleUrls: ['./guide.component.css']
+})
+export class GuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+  test(){
+      localStorage.setItem("login","true");
+
+  }
+}
